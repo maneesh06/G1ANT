e1d61b6d40219e500c2bc86955eae5d8e98bb50e
Maneesh <56780894+maneesh06@users.noreply.github.com>
2020-07-01 11:20:50 +0530

    Add files via upload

---
+++ My_G1ANT_Command.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.MyAddon2
+{
+    [Command(Name ="mynewcommand", Tooltip = "This is a demo command.")]
+    class My_G1ANT_Command : Command
+    {
+        public My_G1ANT_Command(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : CommandArguments
+        {
+            [Argument(Name = "user", Required = true, Tooltip = "Type text to display")]
+            public TextStructure user { get; set; } = new TextStructure();
+        }
+        public void Execute(Arguments argu)
+        {
+            RobotMessageBox.Show(argu.user.Value);
+        }
+    }
+}
